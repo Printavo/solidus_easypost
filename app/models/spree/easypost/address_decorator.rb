c36c73dc4160885cb326114e5272ae056be6d51f
Nick Michalski <Nicholas.A.Michalski@gmail.com>
2020-05-13 14:07:29 -0500

    Adding residential flag, when possible, to EasyPost::Address
    TrackerUrl: https://www.pivotaltracker.com/story/show/172464157

--- app/models/spree/easypost/address_decorator.rb
+++ app/models/spree/easypost/address_decorator.rb
@@ -16,6 +16,7 @@ module Spree
         attributes[:name] = full_name if respond_to?(:full_name)
         attributes[:state] = state ? state.abbr : state_name
         attributes[:country] = country.try(:iso)
+        attributes[:residential] = residential if respond_to?(:residential)
 
         ::EasyPost::Address.create(attributes, easypost_api_key)
       end
